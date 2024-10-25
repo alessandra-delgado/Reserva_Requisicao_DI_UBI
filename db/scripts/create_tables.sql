3316c30e27a0c386fd6ce6d49862f6cc5a9f9570
Alessandra Delgado <alessandra.yuna.delgado@gmail.com>
2024-10-25 11:54:32 +0100

    Id's now increment

--- db/scripts/create_tables.sql
+++ db/scripts/create_tables.sql
@@ -11,34 +11,34 @@ CREATE TABLE Utilizador
   idu VARCHAR(10) NOT NULL UNIQUE,
   id_tipo VARCHAR(2) NOT NULL,
   prioridade_corrente VARCHAR(6),
+  telemovel INT NOT NULL UNIQUE,
   faltas int,
   PRIMARY KEY (idu),
   FOREIGN KEY (id_tipo) REFERENCES Tipo_utilizador(id_tipo)
 			ON Delete No ACTION On UpDate No Action,
 );
 
-CREATE TABLE Email
+CREATE TABLE Contacto
 (
-  idm INT NOT NULL,
-  idu VARCHAR(10) NOT NULL,
+  idu VARCHAR(10) NOT NULL UNIQUE,
   email VARCHAR(50) NOT NULL,
-  PRIMARY KEY (idm),
+  PRIMARY KEY (idu),
   FOREIGN KEY (idu) REFERENCES Utilizador(idu)
 			ON Delete No ACTION On UpDate No Action,
 );
 
 CREATE TABLE Telemovel
 (
-  idt INT NOT NULL,
+  idt INT NOT NULL IDENTITY(1,1),
   idu VARCHAR(10) NOT NULL,
   telemovel INT NOT NULL,
   PRIMARY KEY (idt),
-  FOREIGN KEY (idu) REFERENCES Utilizador(idu)
+  FOREIGN KEY (idu) REFERENCES utilizador(idu)
 );
 
 CREATE TABLE Equipamento
 (
-  ide INT NOT NULL,
+  ide INT NOT NULL IDENTITY(1,1),
   estado VARCHAR(10) NOT NULL,
   nome VARCHAR(50) NOT NULL,
   PRIMARY KEY (ide)
@@ -47,7 +47,7 @@ CREATE TABLE Equipamento
 
 CREATE TABLE Reserva
 (
-  idr INT NOT NULL,
+  idr INT NOT NULL IDENTITY(20240001,1),
   idu VARCHAR(10) NOT NULL,
   data_registo DATE NOT NULL,
   periodo_uso_inicio DATETIME NOT NULL,
@@ -58,10 +58,9 @@ CREATE TABLE Reserva
 			ON Delete No ACTION On UpDate No ACTION,
 );
 
-
 CREATE TABLE Requisicao
 (
-  idq INT NOT NULL,
+  idq INT NOT NULL IDENTITY(1,1),
   idu VARCHAR(10) NOT NULL,
   estado VARCHAR(10) NOT NULL,
   PRIMARY KEY (idq),
@@ -88,5 +87,4 @@ CREATE TABLE RequisicaoPossuiEquipamento
 			ON Delete No ACTION On UpDate Cascade,
   FOREIGN KEY (ide) REFERENCES Equipamento(ide)
 			ON Delete No ACTION On UpDate Cascade,
-      
 );
