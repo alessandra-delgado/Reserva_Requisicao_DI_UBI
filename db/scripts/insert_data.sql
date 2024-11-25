2a8aa236cb0ed696bacc855d0afbd048673e194f
Ana Beatriz Duarte Ferreira da Silva <152163299+AnaBDSilva@users.noreply.github.com>
2024-11-25 22:46:56 +0000

    Update insert_data.sql
    Inseri as prioridades como numeros e as faltas só para testar o trigger faltas

--- db/scripts/insert_data.sql
+++ db/scripts/insert_data.sql
@@ -1,25 +1,25 @@
 INSERT INTO Tipo_utilizador (id_tipo, descricao, prioridade_base) VALUES
-('PD', 'Presidente', 'Máxima'),
-('PR', 'Professor', 'Acima'),
-('RS', 'Investigador', 'Média'),
-('BS', 'Licenciatura', 'Média'),
-('MS', 'Mestrado', 'Média'),
-('DS', 'Doutoramento', 'Média'),
-('SF', 'Apoio', 'Média'),
-('XT', 'Externo', 'Média');
+('PD', 'Presidente', 5),
+('PR', 'Professor', 4),
+('RS', 'Investigador', 3),
+('BS', 'Licenciatura', 3),
+('MS', 'Mestrado', 3),
+('DS', 'Doutoramento', 3),
+('SF', 'Apoio', 3),
+('XT', 'Externo', 3);
 
-INSERT INTO utilizador(idu, id_tipo, telemovel) VALUES
-('PD_Frutuos', 'PD', '274638468'),
-('PR_SPECIAL', 'PR', '123456789'),
-('RS_FABIO', 'RS', '347826592'),
-('DS_DARIO', 'DS', '48527573'),
-('MS_DAVINA', 'MS', '384396001'),
-('BS_Dragon', 'BS', '999028458'),
-('BS_YUNA', 'BS', '448566772'),
-('BS_ANA', 'BS', '999586709'),
-('BS_CAROL', 'BS', '88857472'),
-('SF_BETTEN', 'SF', '848393582'),
-('XT_MONIZ', 'XT', '885748209');
+INSERT INTO utilizador(idu, id_tipo, prioridade_corrente, telemovel, faltas) VALUES
+('PD_Frutuos', 'PD', 5, '274638468', 0),
+('PR_SPECIAL', 'PR', 4, '123456789', 0),
+('RS_FABIO', 'RS', 3, '347826592', 0),
+('DS_DARIO', 'DS', 3, '48527573', 0),
+('MS_DAVINA', 'MS', 3, '384396001', 0),
+('BS_Dragon', 'BS', 3,'999028458', 0),
+('BS_YUNA', 'BS', 3,'448566772', 0),
+('BS_ANA', 'BS', 3, '999586709',0),
+('BS_CAROL', 'BS', 3,  '88857472',0),
+('SF_BETTEN', 'SF', 3, '848393582',0),
+('XT_MONIZ', 'XT', 3, '885748209',0);
 
 INSERT INTO contacto(idu, email) VALUES
 ('PD_Frutuos', 'frutuoso@di.ubi.pt'),
@@ -65,6 +65,27 @@ INSERT INTO reserva (idu, periodo_uso_inicio, periodo_uso_fim, estado) values
 ('BS_CAROL', GETDATE(), GETDATE(), 'Waiting'),
 ('BS_ANA', GETDATE(), GETDATE(), 'Forgotten'),
 ('PR_SPECIAL', GETDATE(), GETDATE(), 'Active');
+
+INSERT INTO PrioridadeTN(id_tipo, num_prioridade) values
+('PD', 5),
+('PR', 4),
+('RS', 3),
+('BS', 3),
+('MS', 3),
+('DS', 3),
+('SF', 3),
+('XT', 3);
+
+INSERT INTO PrioridadeNC(num_prioridade, class_prioridade) values
+(5, 'Máxima'),
+(4, 'Acima'),
+(3, 'Média'),
+(2, 'Abaixo'),
+(1, 'Mínima');
+
+select * from prioridadeNC 
+select * from PrioridadeTN 
+select * from Utilizador
 
 
 -- presidente do departamento deveria ser um tipo de utilizador?
