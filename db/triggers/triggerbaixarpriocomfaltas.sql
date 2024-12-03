915359adf570a6ffeec2c06d30d6cdd68bcc642a
AnaBDSilva <ana.bd.silva@ubi.pt>
2024-12-03 17:53:24 +0000

    trigger mudar prio com faltas para ingles

--- db/triggers/triggerbaixarpriocomfaltas.sql
+++ db/triggers/triggerbaixarpriocomfaltas.sql
@@ -1,29 +1,29 @@
-DROP TRIGGER IF EXISTS MudarPrioridadeComFaltas;
+DROP TRIGGER IF EXISTS ChangePriorityWithMisses;
 GO
-CREATE TRIGGER MudarPrioridadeComFaltas 
-ON Utilizador 
+CREATE TRIGGER ChangePriorityWithMisses 
+ON User_DI 
 AFTER UPDATE 
 AS 
 BEGIN  
 	IF EXISTS ( 
 		SELECT 1 
 		FROM INSERTED 
-		JOIN DELETED ON INSERTED.idu = DELETED.idu 
-		WHERE INSERTED.faltas <> DELETED.faltas 
+		JOIN DELETED ON INSERTED.id_user = DELETED.id_user 
+		WHERE INSERTED.misses <> DELETED.misses 
 	) 
 	--this begin block only runs if the faltas column is updated 
 	BEGIN
 		--atualizar a prioridade e as faltas para alguém que a prioridade ainda é positiva
-		UPDATE Utilizador 
-		SET	prioridade_corrente = prioridade_corrente - 1, 
-		    faltas = 0 
-		WHERE faltas = 5
-		AND prioridade_corrente > 1
+		UPDATE User_DI 
+		SET	current_priority = current_priority - 1, 
+		    misses = 0 
+		WHERE misses = 5
+		AND current_priority > 1
 
 		--atualizar apenas as faltas nas pessoas em que a prioridade já é 0
-		UPDATE Utilizador 
-		SET	faltas = 0 
-		WHERE faltas = 5
-		AND prioridade_corrente = 1
+		UPDATE User_DI 
+		SET	misses = 0 
+		WHERE misses = 5
+		AND current_priority = 1
 	END 
 END
