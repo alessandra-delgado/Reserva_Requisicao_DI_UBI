89e61d71fbc0ebe6d738fe80193b2798689195a3
Alessandra Delgado <alessandra.delgado@ubi.pt>
2024-12-05 12:38:51 +0000

    minor changes in assign to proc

--- db/scripts/TEST_insert_data.sql
+++ db/scripts/TEST_insert_data.sql
@@ -62,22 +62,34 @@ INSERT INTO Reservation (id_user, reg_date, time_start, time_end, status_res)
 VALUES ('BS_YUNA',GETDATE(),GETDATE(), GETDATE(), 'Active');
 SELECT *
 FROM Reservation
-INSERT INTO Res_Equip(id_reserv, id_equip, essential, assigned_to)
-VALUES ('20240001', 13, 'F', 'T'),
-       ('20240001', 14, 'F', 'T'),
-       ('20240001', 15, 'F', 'T'),
-       ('20240001', 16, 'F', 'T'),
-       ('20240001', 17, 'F', 'T'),
-       ('20240001', 21, 'F', 'T');
+
+INSERT INTO Res_Equip(id_reserv, id_equip, essential)
+VALUES ('20240001', 13, 0),
+       ('20240001', 14, 0),
+       ('20240001', 15, 0),
+       ('20240001', 16, 0),
+       ('20240001', 17, 0),
+       ('20240001', 21, 0);
+
+INSERT INTO Res_Equip(id_reserv, id_equip, essential)
+VALUES ('20240002', 1, 1)
+
+INSERT INTO Reservation (id_user, reg_date, time_start, time_end, status_res)
+VALUES ('BS_CAROL',GETDATE(),'2024-12-06 12:29:10.550', '2024-12-07 12:29:10.550', 'Active');
+INSERT INTO Res_Equip(id_reserv, id_equip, essential)
+VALUES ('20240005', 3, 1)
+INSERT INTO Reservation (id_user, reg_date, time_start, time_end, status_res)
+VALUES ('BS_CAROL',GETDATE(),'2024-12-06 04:29:10.550', '2024-12-06 11:29:10.550', 'Active');
+INSERT INTO Res_Equip(id_reserv, id_equip, essential)
+VALUES ('20240006', 4, 1)
 
 
 SELECT *
 FROM Res_Equip
 
 UPDATE Reservation
-SET status_res = 'Satisfied'
-WHERE id_user LIKE 'BS_YUNA'
-  AND id_reserv LIKE '20240001';
+SET status_res = 'Active'
+WHERE  id_reserv LIKE '20240001';
 
 SELECT *
 FROM Req_Equip
