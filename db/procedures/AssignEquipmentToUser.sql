b780741c37692454369ebf854f3a8dcde572a8e6
Alessandra Delgado <alessandra.delgado@ubi.pt>
2024-12-13 23:52:45 +0000

    assignto procedure to develop later...

--- db/procedures/AssignEquipmentToUser.sql
+++ db/procedures/AssignEquipmentToUser.sql
@@ -16,35 +16,5 @@ BEGIN
       AND re.id_reserv = r.id_reserv
       AND r.status_res IN ('Waiting', 'Active')
 
-    SET @countReserva = (SELECT COUNT(re.id_reserv)
-                         FROM Res_Equip AS re,
-                              Reservation AS r
-                         WHERE @ide = id_equip
-                           AND r.status_res IN ('Active', 'Waiting'))
 
-    IF (@countReserva = 1)
-        BEGIN
-            UPDATE Res_Equip
-            SET assigned_to = 1
-            WHERE id_equip = @ide
-            RETURN;
-        END
-
-
-    BEGIN
-        SELECT r.time_start, r.id_reserv FROM Reservation r ORDER BY time_start ASC
-
-
-        --IF ( @countEssencial = 0 )
-
-/*
-        WITH maxPrio AS (SELECT MAX(current_priority)
-                         FROM User_DI u
-                                  INNER JOIN Reservation r ON u.id_user = r.id_user
-                                  INNER JOIN Res_Equip e ON e.id_reserv = r.id_reserv
-                         WHERE e.id_equip = 5
-                           AND essential = 0)*/
-
-
-    END
 END
