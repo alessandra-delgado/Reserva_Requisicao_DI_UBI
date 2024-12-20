6f50d264dcfb6a4b056aaaddb42f5889c541cfd5
AnaBDSilva <ana.bd.silva@ubi.pt>
2024-12-20 20:57:41 +0000

    protected president from misses

--- db/triggers/HitsAndMisses/ReducePriorityOnMissLimit.sql
+++ db/triggers/HitsAndMisses/ReducePriorityOnMissLimit.sql
@@ -11,6 +11,13 @@ BEGIN
                WHERE INSERTED.misses <> DELETED.misses)
         --this begin block only runs if the faltas column is updated
         BEGIN
+		IF EXISTS (
+			SELECT 1
+            FROM INSERTED
+            JOIN DELETED ON INSERTED.id_user = DELETED.id_useR
+			WHERE INSERTED.id_user != 'PR%'
+			)
+			BEGIN
             --atualizar a prioridade e as faltas para alguém que a prioridade ainda é positiva
             UPDATE User_DI
             SET current_priority = current_priority - 1,
@@ -23,5 +30,7 @@ BEGIN
             SET misses = 0
             WHERE misses = 5
               AND current_priority = 1
+
+			END
         END
 END
