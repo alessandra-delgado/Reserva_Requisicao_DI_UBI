8c5aa82595ff4e24fed694dc22151bb6cd53c8fd
Alessandra Delgado <alessandra.delgado@ubi.pt>
2024-12-20 19:41:36 +0000

    omg assign to is real??? (implemented first ver of assign procedure)

--- db/procedures/AssignEquipmentToUser.sql
+++ db/procedures/AssignEquipmentToUser.sql
@@ -3,11 +3,9 @@ GO
 CREATE PROCEDURE AssignEquipmentToUser @ide VARCHAR(10)
 AS
 BEGIN
-    DECLARE @countReserva INT;
-    DECLARE @maxPrio INT;
-    DECLARE @countEssencial INT;
+    DECLARE @IDR INT;
 
-    SELECT u.current_priority, r.status_res, re.essential, re.assigned_to
+    SELECT u.current_priority, r.status_res, re.essential, re.assigned_to, r.id_reserv
     INTO #temp_results
     FROM User_DI u,
          Reservation r,
@@ -15,6 +13,13 @@ BEGIN
     WHERE u.id_user = r.id_user
       AND re.id_reserv = r.id_reserv
       AND r.status_res IN ('Waiting', 'Active')
+    ORDER BY R.time_start DESC, U.current_priority DESC, RE.essential DESC, R.reg_date DESC
 
+
+    SET @IDR = (SELECT TOP 1 id_reserv FROM #temp_results)
+    UPDATE Res_Equip
+        SET assigned_to = 1
+    WHERE id_reserv = @IDR
+      AND id_equip = @ide
 
 END
