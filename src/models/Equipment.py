d4fdcc3636af6c518ed1f3726da2f049df2b5517
Alessandra Delgado <alessandra.delgado@ubi.pt>
2024-12-28 14:42:24 +0000

    Added equipment model

---
+++ src/models/Equipment.py
@@ -0,0 +1,17 @@
+"""
+Requisition table representation in code. Has the queries to Requisition table
+"""
+from models import DataBase as db
+
+
+def add_equipment() -> None:
+    raise NotImplementedError
+
+
+def get_equipments() -> list:
+    conn = db.connect()
+    result = conn.cursor().execute("SELECT * FROM TblEquipment")
+    rows = result.fetchall()
+    db.close(conn)
+
+    return rows
