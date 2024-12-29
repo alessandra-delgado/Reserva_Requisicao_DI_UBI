4db4f2b9261ea739802d81c29e89bbb96acc9e8d
Alessandra Delgado <alessandra.delgado@ubi.pt>
2024-12-29 12:56:44 +0000

    Added new reservation form validations

--- src/views/frames/reserve_new.py
+++ src/views/frames/reserve_new.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 import customtkinter as ctk
-from tktimepicker import constants, SpinTimePickerModern, SpinTimePickerOld
+from tktimepicker import constants, SpinTimePickerOld
 
 from enums.equipmentCategory import EquipmentCategory
 from enums.reservationEquipmentType import ReservationEquipmentType
@@ -51,6 +51,8 @@ class FrameReserveNew(ctk.CTkScrollableFrame):
         self.time_start = SpinTimePickerOld(self.form_frame)
         self.time_start.addAll(constants.HOURS24)
         self.time_start.grid(row=6, column=3, padx=20, pady=(3, 0), sticky="w")
+        self.time_start_error = ctk.CTkLabel(self, text="", text_color="red")
+        self.time_start_error.grid(row=7, column=3, pady=0, padx=20, sticky="w")
 
         # PickEndDate field
         ctk.CTkLabel(self.form_frame, text="Data de Fim").grid(row=8, column=0, padx=20, pady=(20, 0), sticky="w")
@@ -64,30 +66,34 @@ class FrameReserveNew(ctk.CTkScrollableFrame):
         self.time_end = SpinTimePickerOld(self.form_frame)
         self.time_end.addAll(constants.HOURS24)
         self.time_end.grid(row=9, column=3, padx=20, pady=(3, 0), sticky="w")
+        self.time_end_error = ctk.CTkLabel(self, text="", text_color="red")
+        self.time_end_error.grid(row=10, column=3, pady=0, padx=20, sticky="w")
         # EndOf DATETIME pickers -----------------------------------------------------------------------
         # EndOf form_frame ------------------------------------------------------------------------------------------------------
-
 
         # Second frame (scrollable) ---------------------------------------------------------------------------------------------
         self.equipments_radio = {}
         # Select equipments field
+
         ctk.CTkLabel(self, text="Lista de Equipamentos").grid(row=1, column=0, padx=20, pady=(20, 0), sticky="w")
         self.scrollableFrame = ctk.CTkScrollableFrame(self, fg_color="#FFFFFF")
-        self.scrollableFrame.grid(row=3, column=0, sticky="nsew", padx=30, pady=(3, 0))
+        self.scrollableFrame.grid(row=5, column=0, sticky="nsew", padx=30, pady=(3, 0))
         self.scrollableFrame.grid_columnconfigure(4, weight=1)
-
+        self.scrollableFrame_error = ctk.CTkLabel(self, text="", text_color="red")
+        self.scrollableFrame_error.grid(row=6, column=0, pady=0, padx=20, sticky="w")
 
         # EndOf scrollable frame ------------------------------------------------------------------------------------------------
 
         self.category = ctk.StringVar(self, EquipmentCategory.all.value)
 
-        ctk.CTkLabel(self, text="Categoria do Equipamento").grid(row=1, column=0, padx=20, pady=(20, 0), sticky="w")
-        self.combo = ctk.CTkComboBox(self, values=EquipmentCategory.get_categories(), variable=self.category, command=self.reload ,width=300)
-        self.combo.grid(row=2, column=0, pady=(3, 0), padx=20, sticky="w")
+        ctk.CTkLabel(self, text="Categoria do Equipamento").grid(row=3, column=0, padx=20, pady=(20, 0), sticky="w")
+        self.combo = ctk.CTkComboBox(self, values=EquipmentCategory.get_categories(), variable=self.category,
+                                     command=self.reload, width=300)
+        self.combo.grid(row=4, column=0, pady=(3, 0), padx=20, sticky="w")
 
         # Submit button
         self.button = ctk.CTkButton(self, text="Submeter", command=self.submit, width=200)
-        self.button.grid(row=4, column=0, pady=20, padx=20, sticky="e")
+        self.button.grid(row=7, column=0, pady=20, padx=20, sticky="e")
 
     def reload(self, category=None) -> None:
         """ Used by app.py to reload page data. """
@@ -178,16 +184,73 @@ class FrameReserveNew(ctk.CTkScrollableFrame):
         Submits the form.
         """
 
-        equipments_radio = {}
-        for k, v in self.equipments_radio.items():
-            equipments_radio[k] = v.get()
+        if self.is_valid():
+            equipments_radio = {}
+            for k, v in self.equipments_radio.items():
+                equipments_radio[k] = v.get()
 
-        mega_data = self.date_start.get_date() + " " + str(self.time_start.hours24()) + ":" + str(self.time_start.minutes())
-        datetime_start = datetime.strptime(mega_data, "%Y/%m/%d %H:%M")
+            mega_data = self.date_start.get_date() + " " + str(self.time_start.hours24()) + ":" + str(
+                self.time_start.minutes())
+            datetime_start = datetime.strptime(mega_data, "%Y/%m/%d %H:%M")
 
-        mega_data2 = self.date_end.get_date() + " " + str(self.time_end.hours24()) + ":" + str(self.time_end.minutes())
-        datetime_end = datetime.strptime(mega_data2, "%Y/%m/%d %H:%M")
+            mega_data2 = self.date_end.get_date() + " " + str(self.time_end.hours24()) + ":" + str(
+                self.time_end.minutes())
+            datetime_end = datetime.strptime(mega_data2, "%Y/%m/%d %H:%M")
 
-        Reservation.add_reservation(self.user.get(), datetime_start, datetime_end, equipments_radio)
+            Reservation.add_reservation(self.user.get(), datetime_start, datetime_end, equipments_radio)
 
         # Todo: validate date entries
+
+    def is_valid(self) -> bool:
+        """
+        Verify if the form data is valid.
+        :return: True if valid, False otherwise.
+        """
+        valid = True
+
+        # Validate Start Date
+        try:
+            datetime.strptime(self.date_start.get_date(), "%Y/%m/%d")
+            self.date_start_error.configure(text="")
+            self.date_start.configure(border_color="#979DA2")
+        except ValueError:
+            valid = False
+            self.date_start_error.configure(text="Formato inválido.")
+            self.date_start.configure(border_color="red")
+
+        # Validate Start Time
+        try:
+            datetime.strptime(f"{self.time_start.hours24()}:{self.time_start.minutes()}", "%H:%M")
+            self.time_start_error.configure(text="")
+        except ValueError:
+            valid = False
+            self.time_start_error.configure(text="Formato inválido.")
+
+        # Validate End Date
+        try:
+            datetime.strptime(self.date_end.get_date(), "%Y/%m/%d")
+            self.date_end_error.configure(text="")
+            self.date_end.configure(border_color="#979DA2")
+        except ValueError:
+            valid = False
+            self.date_end_error.configure(text="Formato inválido.")
+            self.date_end.configure(border_color="red")
+
+        # Validate Start Time
+        try:
+            datetime.strptime(f"{self.time_end.hours24()}:{self.time_end.minutes()}", "%H:%M")
+            self.time_end_error.configure(text="")
+        except ValueError:
+            valid = False
+            self.time_end_error.configure(text="Formato inválido.")
+
+        valid = False
+        self.scrollableFrame_error.configure(text="Deve selecionar pelo menos um equipamento.")
+        # At least one equipment must be selected
+        for k,v in self.equipments_radio.values():
+            if v.get() in [ReservationEquipmentType.reserved.value, ReservationEquipmentType.essential.value]:
+                valid = True
+                self.scrollableFrame_error.configure(text="")
+                break
+
+        return valid
