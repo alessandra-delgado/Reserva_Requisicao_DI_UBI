4d14050768b4280b7c4238912f17392c53f4abcd
Alessandra Delgado <alessandra.delgado@ubi.pt>
2025-01-02 20:25:01 +0000

    Updated run files

--- db/RUN_LOCAL.bat
+++ db/RUN_LOCAL.bat
@@ -4,24 +4,28 @@ setlocal
 REM Set the directories to search for SQL files
 set "triggers=.\triggers"
 set "procedures=.\procedures"
+set "address=localhost"
+set "database=teste_di"
+set "user=sa"
+set "password=sa"
 
 REM Execute initial SQL scripts
-sqlcmd -S localhost -d teste_di -U sa -P sa -i ".\scripts\DropConstraints.sql"
-sqlcmd -S localhost -d teste_di -U sa -P sa -i ".\scripts\DropTables.sql"
-sqlcmd -S localhost -d teste_di -U sa -P sa -i ".\scripts\CreateTables.sql"
-
+sqlcmd -S "%address%" -d "%database%" -U "%user%" -P "%password%" -i ".\scripts\DropConstraints.sql"
+sqlcmd -S "%address%" -d "%database%" -U "%user%" -P "%password%" -i ".\scripts\DropTables.sql"
+sqlcmd -S "%address%" -d "%database%" -U "%user%" -P "%password%" -i ".\scripts\CreateTables.sql"
 
 REM Loop through each file in the procedures directory and execute them
 for /R "%procedures%" %%f in (*.sql) do (
-    sqlcmd -S localhost -d teste_di -U sa -P sa -i "%%f"
+    sqlcmd -S "%address%" -d "%database%" -U "%user%" -P "%password%" -i "%%f"
 )
 
 REM Loop through each file in the triggers directory and execute them
 for /R "%triggers%" %%f in (*.sql) do (
-    sqlcmd -S localhost -d teste_di -U sa -P sa -i "%%f"
+    sqlcmd -S "%address%" -d "%database%" -U "%user%" -P "%password%" -i "%%f"
 )
 
-sqlcmd -S localhost -d teste_di -U sa -P sa -i ".\scripts\InsertData.sql"
+sqlcmd -S "%address%" -d "%database%" -U "%user%" -P "%password%" -i ".\scripts\SetTriggerOrder.sql"
+sqlcmd -S "%address%" -d "%database%" -U "%user%" -P "%password%" -i ".\scripts\InsertData.sql"
 
 echo Done running all SQL scripts.
 pause
