553e6648ca254d173c83b18827537810b0fede47
Alessandra Delgado <alessandra.delgado@ubi.pt>
2025-01-02 16:24:58 +0000

    Removed PD from UserPriority list if there's a PD user already

--- src/models/UserPriority.py
+++ src/models/UserPriority.py
@@ -7,10 +7,18 @@ def get_user_priorities() -> list:
     result = conn.cursor().execute("SELECT id_type FROM TblUser_Priority")
     rows = result.fetchall()
 
-    db.close(conn)
 
     data = []
     for row in rows:
         data.append(row[0])
 
+
+    # If there's a president then hide PD option
+    result = conn.cursor().execute("SELECT id_user FROM TblUser_DI WHERE id_type = 'PD'")
+
+    if len(result.fetchall()) > 0:
+        data.remove('PD')
+
+
+    db.close(conn)
     return data
